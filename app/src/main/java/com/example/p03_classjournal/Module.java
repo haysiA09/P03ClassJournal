5dc0bf73a0ac727281fa1b2f1687bf93a49e7159
SITI AISYAH BTE HAMDAN <16022877@rp.edu.sg>
2019-05-03 14:04:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p03_classjournal/Module.java
@@ -0,0 +1,18 @@
+package com.example.p03_classjournal;
+
+public class Module {
+    private String code;
+    private String grade;
+    private String week;
+
+    public String getCode(){
+        return code;
+    }
+
+    public String getGrade(){
+        return grade;
+
+    }
+
+    public
+}
